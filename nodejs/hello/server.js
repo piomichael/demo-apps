1e355c996a8d027c31fc5c5a08aaaf8672eee576
b652148 <michael.pio@kp.org>
2020-03-04 23:37:54 -0800

    refactored json

--- nodejs/hello/server.js
+++ nodejs/hello/server.js
@@ -1,17 +1,13 @@
-'use strict';
-
 const express = require('express');
+const os = require('os');
 
-var IP = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var app = express();
 
-// Constants
-const PORT = 8080;
-
-// App
-const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+    res.json({
+        hello: "world"
+    });
+})
 
-app.listen(PORT, IP);
-console.log(`Running on http://${IP}:${PORT}`);
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Server running on ${port}...`));
